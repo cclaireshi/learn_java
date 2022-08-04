27186ae79d77c112941ae71bf78b7d85d011541c
Claire <claire@claires-air.attlocal.net>
2022-08-03 23:19:03 -0700

    added a comment

--- Pascal_Triangle2.java
+++ Pascal_Triangle2.java
@@ -22,6 +22,9 @@ public class Pascal_Triangle2 {
             pascalTriangle[i] = new int[i+1];
         }
 
+        /*
+         * fill up the triangle
+         */
         for (int i = 0; i < triangleSize; i++) {
             for (int j = 0; j < i+1; j++) {
                 if (i == j || j == 0) {
@@ -30,7 +33,7 @@ public class Pascal_Triangle2 {
                 else {
                     pascalTriangle[i][j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j]; 
                 }
-                
+
             }
         }
         return pascalTriangle; 
